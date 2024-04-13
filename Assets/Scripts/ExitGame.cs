1c4bfa24c5230c835d7f2c12dc6963436b5e4e02
Gindila Teodor <teodorgindila@gmail.com>
2024-04-13 13:46:20 +0300

    Added finish, death and a menu(?)

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnMyButtonClick()
+    {
+        Application.Quit();
+
+    }
+    public void Start()
+    {
+        SceneManager.LoadScene("level1");
+    }
+}
--- Assets/Scripts/ObjectCollider.cs
+++ Assets/Scripts/ObjectCollider.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObjectCollider : MonoBehaviour
 {
@@ -20,8 +21,8 @@ public class ObjectCollider : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        
-        if (collision.gameObject.tag== "Portal1" && time>3)
+
+        if (collision.gameObject.tag == "Portal1" && time > 3)
         {
             // Update the reference to Portal2
             portal2 = GameObject.FindGameObjectWithTag("Portal2");
@@ -29,7 +30,13 @@ public class ObjectCollider : MonoBehaviour
             if (portal2 != null)
             {
                 // Teleport the player to the position of Portal2
-                transform.position = portal2.transform.position;
+                Vector3 directionFromPortal = portal2.transform.forward;
+
+                // Calculate the position in front of the portal
+                Vector3 newPosition = portal2.transform.position + (directionFromPortal * 10);
+
+                // Teleport the player to the new position
+                transform.position = newPosition; ;
             }
             portal2 = GameObject.FindGameObjectWithTag("Portal2Ground");
 
@@ -58,8 +65,13 @@ public class ObjectCollider : MonoBehaviour
 
             if (portal2 != null)
             {
-                // Teleport the player to the position of Portal2
-                transform.position = portal2.transform.position;
+                Vector3 directionFromPortal = portal2.transform.forward;
+
+                // Calculate the position in front of the portal
+                Vector3 newPosition = portal2.transform.position + (directionFromPortal * 10);
+
+                // Teleport the player to the new position
+                transform.position = newPosition;
             }
             portal2 = GameObject.FindGameObjectWithTag("Portal2Ground");
 
@@ -88,8 +100,13 @@ public class ObjectCollider : MonoBehaviour
 
             if (portal2 != null)
             {
-                // Teleport the player to the position of Portal2
-                transform.position = portal2.transform.position;
+                Vector3 directionFromPortal = portal2.transform.forward;
+
+                // Calculate the position in front of the portal
+                Vector3 newPosition = portal2.transform.position + (directionFromPortal * 10);
+
+                // Teleport the player to the new position
+                transform.position = newPosition;
             }
             portal2 = GameObject.FindGameObjectWithTag("Portal2Ground");
 
@@ -118,8 +135,13 @@ public class ObjectCollider : MonoBehaviour
 
             if (portal2 != null)
             {
-                // Teleport the player to the position of Portal2
-                transform.position = portal2.transform.position;
+                Vector3 directionFromPortal = portal2.transform.forward;
+
+                // Calculate the position in front of the portal
+                Vector3 newPosition = portal2.transform.position + (directionFromPortal * 10);
+
+                // Teleport the player to the new position
+                transform.position = newPosition;
             }
             portal2 = GameObject.FindGameObjectWithTag("Portal1Ground");
 
@@ -148,8 +170,13 @@ public class ObjectCollider : MonoBehaviour
 
             if (portal2 != null)
             {
-                // Teleport the player to the position of Portal2
-                transform.position = portal2.transform.position;
+                Vector3 directionFromPortal = portal2.transform.forward;
+
+                // Calculate the position in front of the portal
+                Vector3 newPosition = portal2.transform.position + (directionFromPortal * 10);
+
+                // Teleport the player to the new position
+                transform.position = newPosition;
             }
             portal2 = GameObject.FindGameObjectWithTag("Portal1Ground");
 
@@ -179,7 +206,13 @@ public class ObjectCollider : MonoBehaviour
             if (portal2 != null)
             {
                 // Teleport the player to the position of Portal2
-                transform.position = portal2.transform.position;
+                Vector3 directionFromPortal = portal2.transform.forward;
+
+                // Calculate the position in front of the portal
+                Vector3 newPosition = portal2.transform.position + (directionFromPortal * 10);
+
+                // Teleport the player to the new position
+                transform.position = newPosition;
             }
             portal2 = GameObject.FindGameObjectWithTag("Portal1Ground");
 
@@ -201,7 +234,14 @@ public class ObjectCollider : MonoBehaviour
             }
             time = 0;
         }
-        
+        if (collision.gameObject.tag == "Death")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        if (collision.gameObject.tag == "Finish")
+        {
+            SceneManager.LoadScene("StartScene");
+        }
     }
 }
 
